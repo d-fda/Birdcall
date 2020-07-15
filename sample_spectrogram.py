7617e812c300fbbcf6ca7dd82bcbca0cad6568c4
David D'Amario <damario.david@gmail.com>
2020-07-14 21:54:46 -0400

    clip specs 5s at 43sr

--- sample_spectrogram.py
+++ sample_spectrogram.py
@@ -10,21 +10,35 @@ sr = 43
 # 5s clip length
 clip_length = int(sr*5)
 
+# read in spectrogram of mp3s
 df = pd.read_pickle(samples_path)
 
-X = []
-y = []
+df_clipped = []
 for i in tqdm(range(len(df))):
     mel_spec = df.iloc[i]['mel spec']
     bird = df.iloc[i]['bird']
+    filename = df.iloc[i]['id']
+
+    # calculate how many 5s clips are in the file
     n_clips = int(len(mel_spec[0][:])/clip_length)
+
+    n = 0
+    # j is start index of clip to be sliced
     for j in range(0, n_clips*clip_length, clip_length):
         clip = mel_spec[:, j:j+clip_length]
-        X.append(clip)
-        y.append(bird)
+        # expand to make shape (l,w,d) d = 1
+        clip = np.expand_dims(clip, axis=2)
+        # populate row of dataframe, fileid marks mp3 filename AND start time (start time=n*clip_length)
+        row = {'mel spec': clip,
+               'bird': bird,
+               'fileid': filename + '_' + str(n)}
+        n += 1
+        df_clipped.append(row)
 
-X = np.array(X)
-y = np.array(y)
+# save list to df
+df_clipped = pd.DataFrame(df_clipped)
 
-print(type(X))
-print(len(X))
+# write to memory
+df_clipped.to_pickle(r"C:\Users\David D'Amario\Data\Birdcalls\spectrogram_samples.pkl")
+
+print(len(df_clipped))
